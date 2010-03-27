45d19e86a78163725f79e14bb4584de694d54790
Bryce Kerley <bkerley@brycekerley.net>
2010-03-27 12:25:37 +0000

    middleware load path

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module Tooter
 
     # Add additional load paths for your own custom dirs
     # config.load_paths += %W( #{config.root}/extras )
+    config.load_paths += %W( #{config.root}/app/middleware )
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named
